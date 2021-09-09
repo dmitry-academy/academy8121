2c01d4d857bcab82ef5b2eae19a48d7a004d2a62
Dzmitry Shchepin <Dzmitry.Shchepin@gmail.com>
2021-09-09 17:29:23 +0300

    lesson19

---
+++ academy8121/src/by/academy/lesson19/MyRunnable.java
@@ -0,0 +1,11 @@
+package by.academy.lesson19;
+
+public class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		
+		
+		System.out.println("Важная работа выполняется.");
+	}
+}
